360ca5313fe566415e948ab9146f022e7f5b7bf2
Preethika776 <23124043@srcas.ac.in>
2025-03-14 14:54:55 +0530

    push

---
+++ Armstrong.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Armstrong 
+ {
+  public static void main(String args[])  
+  {
+   Scanner scanner = new Scanner(System.in);
+   int n = scanner.nextInt();          
+   int s = 0, t = n; 
+        while (t > 0)
+          {
+            int digit = t % 10; 
+            s += (int) Math.pow(digit, 3); 
+            t /= 10;  // Remove last digit
+        }
+
+        System.out.println(n == s ? "Armstrong" : "Not Armstrong"); 
+    }
+}
